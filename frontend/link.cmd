1475a02b5c4908c3506a6895ebeeeff13e744b44
Noel.qazim <noel.qazim2006@gmail.com>
2026-05-07 17:39:07 +0200

    liten link grej

---
+++ frontend/link.cmd
@@ -0,0 +1,7 @@
+@echo off
+echo.
+echo PHP server links:
+echo http://localhost:8080/index.php
+echo http://localhost:8080/login-signup.php
+echo http://localhost:8080/nyheter.php
+start http://localhost:8080/index.php
